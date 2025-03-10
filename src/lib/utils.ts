67c8ca2cb650d9cd47db3550a23bc377bc5b5804
Nathan Laundry <me@nathanlaundry.com>
2025-03-10 12:03:32 -0400

    Initial testing

---
+++ src/lib/utils.ts
@@ -0,0 +1,97 @@
+import fs from "node:fs/promises";
+import { GLOBAL } from "./variables";
+
+type MarkdownData<T extends object> = {
+    frontmatter: T;
+    file: string;
+    url: string;
+};
+
+enum ContentType {
+    Projects = "projects",
+    Blog = "blog",
+    Poetry = "poetry",
+    Publications = "publications",
+}
+
+const fileGlobs: Record<ContentType, Record<string, () => Promise<any>>> = {
+    [ContentType.Projects]: import.meta.glob("/src/pages/projects/*.md"),
+    [ContentType.Blog]: import.meta.glob("/src/pages/blog/*.md"),
+    [ContentType.Poetry]: import.meta.glob("/src/pages/poetry/*.md"),
+    [ContentType.Publications]: import.meta.glob("/src/pages/publications/*.md"),
+};
+
+/**
+ * This function processes the content of a directory and returns an array of processed content.
+ * It takes a content type, a function to process the content, and an optional directory.
+ * If no directory is provided, it defaults to the current working directory.
+ * 
+ * @param contentType the type of content to process
+ * @param processFn the function to process the content
+ * @param dir the directory to process the content from
+ * @returns a promise that resolves to an array of processed content
+ */
+export const processContentInDir = async <T extends object, K>(
+    contentType: ContentType,
+    processFn: (data: MarkdownData<T>) => K,
+    dir: string = process.cwd(),
+) => {
+    const files = await fs.readdir(`${dir}/src/pages/${contentType}`);
+    const markdownFiles = files
+        .filter((file: string) => file.endsWith(".md"))
+        .map((file) => file.split(".")[0]);
+
+    const readMdFileContent = async (file: string) => {
+        const files = fileGlobs[contentType];
+        const filePath = `/src/pages/${contentType}/${file}.md`;
+
+        if (!(filePath in files)) {
+            throw new Error(`File not found: ${filePath}`);
+        }
+
+        const content = await files[filePath]();
+        const data = content as MarkdownData<T>;
+        return processFn(data);
+    };
+
+    return await Promise.all(markdownFiles.map(readMdFileContent));
+};
+
+/**
+ * Shortens a string by removing words at the end until it fits within a certain length.
+ * @param content the content to shorten
+ * @param maxLength the maximum length of the shortened content (default is 20)
+ * @returns a shortened version of the content
+ */
+export const getShortDescription = (content: string, maxLength = 20) => {
+    const splitByWord = content.split(" ");
+    const length = splitByWord.length;
+    return length > maxLength ? splitByWord.slice(0, maxLength).join(" ") + "..." : content;
+};
+
+/**
+ * Processes the date of an article and returns a string representing the processed date.
+ * @param timestamp the timestamp to process
+ * @returns a string representing the processed timestamp
+ */
+export const processArticleDate = (timestamp: string) => {
+    const date = new Date(timestamp);
+    const monthSmall = date.toLocaleString("default", { month: "short" });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${monthSmall} ${day}, ${year}`;
+};
+
+/**
+ * Generates a source URL for a content item. The URL is used in meta tags and social media cards.
+ * @param sourceUrl the source URL of the content
+ * @param contentType the type of content
+ * @returns a string representing the source URL with the appropriate domain
+ */
+export const generateSourceUrl = (
+    sourceUrl: string,
+    contentType: ContentType,
+) => {
+    return `${GLOBAL.rootUrl}/${contentType}/${sourceUrl}`;
+};
+
---
+++ src/lib/variables.ts
@@ -0,0 +1,59 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+    // Site metadata
+    username: "Nathan Laundry",
+    rootUrl: "https://nathanlaundry.com",
+    shortDescription: "On Life and Lab",
+    longDescription: "Reformed productivity bro exploring the Good life through philosophy and psychology | enjoying linux and the homelab along the way",
+
+    // Social media links
+    githubProfile: "https://github.com/RATIU5/zaggonaut",
+    twitterProfile: "https://twitter.com/",
+
+    // Common text names used throughout the site
+    articlesName: "Articles",
+    projectsName: "Projects",
+    poetryName: "Poetry",
+    publicationsName: "Publication",
+    viewAll: "View All",
+
+    // Common descriptions used throughout the site
+    noArticles: "No featured articles yet.",
+    noProjects: "No featured projects yet.",
+    noPoetry: "No featured poetry yet.",
+    noPublications: "No featured publications yet.",
+
+    // Blog metadata
+    blogTitle: "On Life and Lab",
+    blogShortDescription: "Trying to live a good life and enjoy linuxy/homelaby things",
+    blogLongDescription: "Reformed productivity bro exploring the Good life through philosophy and psychology | enjoying linux and the homelab along the way",
+
+    // Project metadata
+    projectTitle: "Projects and Code",
+    projectShortDescription: "",
+    projectLongDescription: "All of my projects, including both frontend and full-stack applications.",
+
+    // poetry metadata
+    poetryTitle: "You gotta dump your baggage somewhere",
+    poetryShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+    poetryLongDescription: "Web development, tech trends, and the occasional programming mishap.",
+    
+    // publication metadata
+    publicationTitle: "My Thoughts & Takes",
+    publicationShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+    publicationLongDescription: "Web development, tech trends, and the occasional programming mishap.",
+
+
+    // Profile image
+    profileImage: "michael-dam-unsplash.webp",
+
+    // Menu items
+    menu: {
+        home: "/",
+        projects: "/projects",
+        blog: "/blog",
+        poetry: "/poetry",
+        publications: "/publications",
+    }
+};
